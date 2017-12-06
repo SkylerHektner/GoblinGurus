dbe15f3de71a04adeb8f53ee275b105310c3d2bf
Xavier Humberg <u0893045@utah.edu>
2017-12-05 18:23:14 -0700

    Added some tier 2 and 3 questions

--- build-3505_Final_Project-Desktop_Qt_5_9_2_MinGW_32bit-Debug/debug/moc_gamecontroller.cpp
+++ build-3505_Final_Project-Desktop_Qt_5_9_2_MinGW_32bit-Debug/debug/moc_gamecontroller.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../gamecontroller.h"
+#include "../../3505_Final_Project/gamecontroller.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
