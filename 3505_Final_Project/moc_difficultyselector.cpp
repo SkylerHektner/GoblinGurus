2bd16cc8962c20d2218c9e10f11ca3091b0bba96
Unknown <trevor.overby4@gmail.com>
2017-12-14 18:42:02 -0700

    Fixing more things. AI doesnt seem to display

--- 3505_Final_Project/moc_difficultyselector.cpp
+++ 3505_Final_Project/moc_difficultyselector.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_difficultyselector_t {
-    QByteArrayData data[5];
-    char stringdata0[56];
+    QByteArrayData data[7];
+    char stringdata0[94];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -35,11 +35,14 @@ QT_MOC_LITERAL(0, 0, 18), // "difficultyselector"
 QT_MOC_LITERAL(1, 19, 8), // "playGame"
 QT_MOC_LITERAL(2, 28, 0), // ""
 QT_MOC_LITERAL(3, 29, 4), // "diff"
-QT_MOC_LITERAL(4, 34, 21) // "on_PlayButton_clicked"
+QT_MOC_LITERAL(4, 34, 21), // "on_PlayButton_clicked"
+QT_MOC_LITERAL(5, 56, 30), // "on_difficultyselector_finished"
+QT_MOC_LITERAL(6, 87, 6) // "result"
 
     },
     "difficultyselector\0playGame\0\0diff\0"
-    "on_PlayButton_clicked"
+    "on_PlayButton_clicked\0"
+    "on_difficultyselector_finished\0result"
 };
 #undef QT_MOC_LITERAL
 
@@ -49,7 +52,7 @@ static const uint qt_meta_data_difficultyselector[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -57,16 +60,18 @@ static const uint qt_meta_data_difficultyselector[] = {
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    1,   24,    2, 0x06 /* Public */,
+       1,    1,   29,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       4,    0,   27,    2, 0x08 /* Private */,
+       4,    0,   32,    2, 0x08 /* Private */,
+       5,    1,   33,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void, QMetaType::Int,    3,
 
  // slots: parameters
     QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    6,
 
        0        // eod
 };
@@ -79,6 +84,7 @@ void difficultyselector::qt_static_metacall(QObject *_o, QMetaObject::Call _c, i
         switch (_id) {
         case 0: _t->playGame((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 1: _t->on_PlayButton_clicked(); break;
+        case 2: _t->on_difficultyselector_finished((*reinterpret_cast< int(*)>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -119,13 +125,13 @@ int difficultyselector::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 3;
     }
     return _id;
 }
